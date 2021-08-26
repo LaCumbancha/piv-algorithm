7da468f5d7ef7bc7e8cb3754af87f98841c00ab2
Matias Rozanec <rozanecm@gmail.com>
2021-08-25 22:37:10 -0300

    document tmp dir need

--- octave/octave.py
+++ octave/octave.py
@@ -3,6 +3,8 @@ import os
 
 from oct2py import Oct2Py
 
+# tmp dir needs to be specified, else it'll break
+# https://stackoverflow.com/a/63406242
 temp_folder = os.path.join(os.getcwd(), "tmp")
 os.makedirs(temp_folder, exist_ok=True)
 octave_cli = Oct2Py(temp_dir=temp_folder)
