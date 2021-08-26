7619812d8f1321878c7953243bf835500bc1f046
Cristian Raña <cerana@fi.uba.ar>
2021-08-25 23:31:16 -0300

    Gitignore updated

--- octave/octave.py
+++ octave/octave.py
@@ -1,11 +1,10 @@
 # Imports
-import os
 
+import os
 from oct2py import Oct2Py
 
-# tmp dir needs to be specified, else it'll break
-# https://stackoverflow.com/a/63406242
-temp_folder = os.path.join(os.getcwd(), "tmp")
+# The octave-tmp directory needs to be specified, to avoid an Octave issue: https://stackoverflow.com/a/63406242
+temp_folder = os.path.join(os.getcwd(), ".octave-tmp")
 os.makedirs(temp_folder, exist_ok=True)
 octave_cli = Oct2Py(temp_dir=temp_folder)
 octave_cli.addpath('./matlab/')
